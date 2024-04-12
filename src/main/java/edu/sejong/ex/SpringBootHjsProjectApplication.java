8205f101abcdcd166406760ae52ef179aacf0245
admin <admin@admin.com>
2024-04-12 12:06:23 +0900

    first commit

---
+++ src/main/java/edu/sejong/ex/SpringBootHjsProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.sejong.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHjsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHjsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sejong/ex/SpringBootHjsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sejong.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHjsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
