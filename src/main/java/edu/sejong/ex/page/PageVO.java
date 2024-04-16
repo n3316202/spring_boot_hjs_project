77a24ea21c885e691673f39fb96ffd4b0a2ecacb
admin <admin@admin.com>
2024-04-16 16:51:04 +0900

    commit

--- src/main/java/edu/sejong/ex/page/PageVO.java
+++ src/main/java/edu/sejong/ex/page/PageVO.java
@@ -61,8 +61,8 @@ public class PageVO {
 		
 		UriComponents uriComponentsBuilder = 
 				UriComponentsBuilder.newInstance()
-				.queryParam("pageNum", page) // pageNum =// 3
-				.queryParam("amount", criteria.getAmount()) // pageNum=3&amount=10
+				.queryParam("pageNum", page) // pageNum = 1
+				.queryParam("amount", criteria.getAmount()) // pageNum=1&amount=10
 				.build(); // ?pageNum=3&amount=10
 		
 		return uriComponentsBuilder.toUriString(); // ?pageNum=3&amount=10 리턴
